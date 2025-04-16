5c10f12af93f4634be1ddd5cac305a0ce8408720
annapons10 <annapons101435@gmail.com>
2025-04-16 12:39:30 +0200

    Add lazy loading para el modal más estilos modal

---
+++ src/components/ModalSeries.jsx
@@ -0,0 +1,48 @@
+import { useRating } from '../hooks/useRating';
+//Icons estrellas: 
+import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import Rating from 'react-rating';
+
+
+export default function ModalSeries({ setIsModalOpen, serieSelected}) {
+    //Custom hook para las clasificaciones:
+    const { rating, handleRatingChange } = useRating({ serieSelected });
+
+    return (
+        <>
+            {/* Fondo transparente: */}
+            <div className='fondo-transparent'></div>
+            <div className='modal-series' > {/* Este es el contenedor principal */}
+                <div className="modal" style={{ display: 'block' }} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="false">
+                    <div className="modal-dialog">
+                        <div className="modal-content modal-container">
+                            <div className="modal-header">
+                                <h1 className="h1" >{serieSelected.name}</h1>
+                                <button type="button" className=" btn-close-modal" data-bs-dismiss="modal" aria-label="Close"
+                                    onClick={() => setIsModalOpen(false)}> X </button>
+                            </div>
+                            <div className="modal-body">
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w200${serieSelected.poster}`}
+                                    alt={`Poster de ${serieSelected.name}`}
+                                />
+                                <p className='p-overview'> {serieSelected.overview} </p>
+                                <p className='p-votos'> Media: {serieSelected.average} </p>
+                                <p> Votos: {serieSelected.count} </p>
+
+                                {/* Las estrellas para votar. */}
+                                {/* Se verá lo que hay en estado gestionado por el useEffect, cada vez que cambie, se llamará al método para actualizar el localStorage:  */}
+                                <Rating initialRating={rating} onChange={handleRatingChange}
+                                    emptySymbol={<FaRegStar size={24} />}
+                                    fullSymbol={<FaStar size={24} />} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+
+}
--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -1,15 +1,14 @@
-import { useState,  useEffect } from 'react'; 
-import Rating from 'react-rating';
-import { useRating } from '../hooks/useRating';
-//Icons estrellas: 
-import { FaRegStar } from "react-icons/fa";
-import { FaStar } from "react-icons/fa";
+import { useState } from 'react'; 
+
+//Para el react.lazy y usarlo en el modal:
+import { Suspense, lazy } from 'react';
+//Dice: es un componente perezoso, el archivo se guarda en la constante y se descarga cuando sea necesario: 
+const  ModalSeries = lazy(() => import('./ModalSeries')); 
 
 export function Series ({ seriesEncontradas, handleButtonInfo, serieSelected }) {
    //Estados para controlar el modal:
    const [isModalOpen, setIsModalOpen] = useState(false);
-   //Custom hook para las clasificaciones:
-   const { rating, handleRatingChange } = useRating({ serieSelected });
+ 
    
     return(
         <> 
@@ -32,41 +31,12 @@ export function Series ({ seriesEncontradas, handleButtonInfo, serieSelected }) 
 
             </ul>
             {/* Recibo la key de la serie clickada: */}
-            { isModalOpen && serieSelected && (
-                <>
-               {/* Fondo transparente: */}
-                <div className='fondo-transparent'></div>
-                <div > {/* Este es el contenedor principal */}
-                    <div className="modal" style={{ display: 'block' }} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="false">
-                    <div className="modal-dialog">
-                        <div className="modal-content modal-container">
-                        <div className="modal-header">
-                            <h1 className="h1" >{ serieSelected.name }</h1>
-                            <button type="button" className=" btn-close-modal" data-bs-dismiss="modal" aria-label="Close"
-                            onClick={ () => setIsModalOpen(false) }> X </button>
-                        </div>
-                        <div className="modal-body">
-                            <img 
-                                src={`https://image.tmdb.org/t/p/w200${serieSelected.poster}`} 
-                                alt={`Poster de ${ serieSelected.name }`} 
-                            />
-                            <p className='p-overview'> { serieSelected.overview } </p>
-                            <p className='p-votos'> Media: {serieSelected.average } </p>
-                            <p> Votos: { serieSelected.count } </p>
-
-                            {/* Las estrellas para votar. */}
-                            {/* Se verá lo que hay en estado gestionado por el useEffect, cada vez que cambie, se llamará al método para actualizar el localStorage:  */}  
-                            <Rating initialRating={ rating } onChange={ handleRatingChange } 
-                             emptySymbol= { <FaRegStar  size={24}/>  }
-                             fullSymbol= { <FaStar size={24} /> }/>
-                        </div> 
-                        </div>
-                    </div>
-                    </div>
-                
-                </div> 
-                </>
-            )}
+            { isModalOpen && serieSelected && 
+                <Suspense fallback={(<div className='loader'>Cargando modal...</div>)}>
+                    <ModalSeries setIsModalOpen={setIsModalOpen} 
+                    serieSelected={serieSelected}/ >
+                </Suspense>
+            }
         </> 
     )
 } 
